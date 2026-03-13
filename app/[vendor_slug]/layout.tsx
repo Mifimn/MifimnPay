da478ba57718ecfa66de45a32fefbfbe01b4fc0b
Musa ayoola shittu <109434707+Mifimn@users.noreply.github.com>
2026-03-13 14:13:41 +0000

    Done

--- app/[vendor_slug]/layout.tsx
+++ app/[vendor_slug]/layout.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React, { useEffect } from 'react';
-import "@/styles/globals.css"; // ADD THIS IMPORT
+import "@/styles/globals.css";
 import BrandHeader from '@/storefront/components/Showroom/BrandHeader';
 import CartDrawer from '@/storefront/components/Showroom/CartDrawer';
 import ModalController from '@/storefront/components/Global/ModalController';
+// REMOVE THIS: import MobileNavbar from '@/storefront/components/Showroom/MobileNavbar'; 
 import InquiryBasket from '@/storefront/components/Showroom/InquiryBasket';
 import { useThemeStore } from '@/storefront/store/useThemeStore';
 import { useCartStore } from '@/storefront/store/useCartStore';
@@ -26,6 +27,8 @@ export default function StorefrontLayout({
       <main className="bg-white dark:bg-[#0a0a0a] min-h-screen relative flex flex-col pb-24 lg:pb-0">
         <BrandHeader />
         <CartDrawer />
+        <ModalController />
+
         <div className="hidden lg:block">
            {basket.length > 0 && (
              <InquiryBasket 
@@ -34,7 +37,9 @@ export default function StorefrontLayout({
              />
            )}
         </div>
-        <MobileNavbar />
+
+        {/* REMOVE THIS COMPONENT TAG: <MobileNavbar /> */}
+
         <div className="flex-1 relative">
           {children}
         </div>
